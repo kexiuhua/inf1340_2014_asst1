399fb84fd1a68b60b2e8f7a98150927f4d1632c5
kexiuhua <xkeileen@gmail.com>
2014-10-05 01:41:20 -0400

    test commit 2

--- exercise3.py
+++ exercise3.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-
 def decide_rps(player1, player2):
     #write something here
     #abc123
-    return 1
+    scores = {("Rock", "Rock"): 0,
+              ("Rock", "Paper"): 2,
+              ("Rock", "Scissors"): 1,
+              ("Paper", "Rock"): 1,
+              ("Paper", "Paper"): 0,
+              ("Paper", "Scissors"): 2,
+              ("Scissors", "Rock"): 2,
+              ("Scissors", "Paper"): 1,
+              ("Scissors", "Scissors"): 0}
 
+    index = (player1, player2)
+
+    return scores[index]
+
+print(decide_rps("Scissors", "Rock"))
+print(decide_rps("Scissors", "asdf"))
